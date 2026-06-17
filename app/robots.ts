7a9c90755f1c11ec4b2b4ad944588cec94c93547
Alberto Landa <52215286+SirkoBot007@users.noreply.github.com>
2026-06-17 16:37:34 +0200

    Update robots.txt to allow EN blog articles
    Removed disallow rule for EN blog article pages.

--- app/robots.ts
+++ app/robots.ts
@@ -8,8 +8,6 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        // Disallow EN blog article pages (they redirect to ES, but belt-and-suspenders)
-        disallow: ['/en/blog/'],
       },
     ],
     sitemap: `${BASE_URL}/sitemap.xml`,
